76f5260030ea5ea2a27de20f3d5313ab0c2fac07
Bradley Rispone <bradrispone@gmail.com>
2023-03-30 22:24:10 -0500

    Add event listeners for clicking on the grid

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Gameboard module
-const Gameboard = (function() {
+const gameboard = (function() {
     // Gameboard array
-    const gameboard = [
+    const board = [
         document.querySelector("#grid-1"),
         document.querySelector("#grid-2"),
         document.querySelector("#grid-3"),
@@ -13,30 +13,40 @@ const Gameboard = (function() {
         document.querySelector("#grid-9")
     ];
 
+    // Add EventListeners for clicking the grids
+    board.forEach((grid, index) => {
+        grid.addEventListener("click", () => markMove(index));
+    });
+
     // Check if position is taken
     const isTaken = function(position) {
-        if(gameboard[position].innerHTML) {
+        if(board[position].innerHTML) {
             return true;
         } else return false;
     }
 
     // Mark position method
-    const markMove = function(position, marker) {
+    const markMove = function(position) {
         if(isTaken(position)) {
             console.log("That position is taken");
             return;
         }
-        gameboard[position].innerHTML = marker;
+        board[position].innerHTML = Game.currentPlayer.marker;
     };
-    
+
     // Check for win method
 
-    return { markMove };
+    return { isTaken, markMove };
 }) ();
 
 // Game flow module
     // Whose turn  is it?
-
+const Game = {
+    currentPlayer: {
+        marker: "X"
+    }
+    
+};
 
 // Player factory
     // Player name
