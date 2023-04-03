4772c2050ef7b07d612fcd0b94f0b271b99913ff
Bradley Rispone <bradrispone@gmail.com>
2023-04-03 16:18:45 -0500

    Add active player highlighting

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const gameboard = (function() {
         });
     };
 
+
     // Check if position is taken
     const isTaken = function(position) {
         if(Game.state[position]) {
@@ -34,6 +35,9 @@ const gameboard = (function() {
 
     // Mark position method
     const markMove = function(position) {
+
+        if (!Game.isActive) { return; } // do nothing if there is not currently a game being played
+
         if(isTaken(position)) {
             alert("That position is taken");
             return;
@@ -51,20 +55,24 @@ const gameboard = (function() {
 // Player factory
     // Is bot?
 
-const Player = (name, marker, winCount) => {
-    return { name, marker, winCount };
+const Player = (name, display, marker, winCount) => {
+    return { name, display, marker, winCount };
 }
 
-const player1 = Player("Player 1", "X", 0);
+const player1 = Player("Player 1", document.querySelector("#player1-info"), "X", 0);
 
-const player2 = Player("Player 2", "O", 0);
+const player2 = Player("Player 2", document.querySelector("#player2-info"), "O", 0);
 
 // Game flow module
     // Whose turn  is it?
 const Game = (function() {
         let currentPlayer = player1;
+        let isActive = false;
     
         const changeTurns = function() {
+
+            player1.display.classList.toggle("active");
+            player2.display.classList.toggle("active");
             if(Game.currentPlayer === player1) {
                 Game.currentPlayer = player2;
             } else Game.currentPlayer = player1;
@@ -82,7 +90,7 @@ const Game = (function() {
                 ((state[0] === state [4]) && (state[4] === state[8]) && state[0]) || // top left to bottom right diagonal
                 ((state[2] === state [4]) && (state[4] === state[6]) && state[2])    // top right to bottom left diagonal
             ) {
-                endGame(currentPlayer);
+                endGame(Game.currentPlayer);
             } else if (state.every(Boolean)) { // returns true if every position on the board is taken already - ie, checks for tie
                 endGame();
             }
@@ -92,12 +100,48 @@ const Game = (function() {
         const endGame = function(winner) { // will end game, award point to winner and display appropriate message. Tie if no argument is passed
             if(winner) {
                 console.log(`${winner.name} wins!`);
+                winner.winCount++;
+                //Scoreboard.update();
             } else {
                 console.log("It's a tie!");
             }
+            Game.currentPlayer.display.classList.toggle("active");
+            Game.isActive = false;
         };
 
-        return { currentPlayer, state, changeTurns, checkForWin };
+        // Event listener for new button game - resets current game, won't start a game if there are not 2 players initialized
+
+        document.querySelector("#new-game-btn").addEventListener("click", (event) => {
+
+            for(let i = state.length; i >= 0; i--) { // clear the gamestate
+                state[i] = "";
+            }
+            gameboard.render();
+            Game.currentPlayer.display.classList.toggle("active");
+            Game.isActive = true;
+        });
+
+        return { currentPlayer, state, isActive, changeTurns, checkForWin };
         
 }) ();
-    
+/*
+const Scoreboard = (function() {
+
+    const playerOneName = document.querySelector("#p1-name");
+    const playerOneMarker = document.querySelector("#p1-marker");
+    const playerOneWins = document.querySelector("#p1-wins");
+    const playerTwoName = document.querySelector("#p2-name");
+    const playerTwoMarker = document.querySelector("#p2-marker");
+    const playerTwoWins = document.querySelector("#p2-wins");
+
+    const update = function() {
+        Scoreboard.playerOneName.innerHTML(player1.name);
+        Scoreboard.playerOneMarker.innerHTML(player1.marker);
+        Scoreboard.playerOneWins.innerHTML(player1.wins);
+        Scoreboard.playerTwoName.innerHTML(player2.name);
+        Scoreboard.playerTwoMarker.innerHTML(player2.marker);
+        Scoreboard.playerTwoWins.innerHTML(player2.wins);
+    };
+
+    return { update };
+}) (); */
